580b9ee6f4d7c5dca0ac999cfb6104073b5c85d3
Roman <obiwan091@yandex.ru>
2023-07-31 21:26:14 +0300

    Add Sum primer

---
+++ Lesson_1_3_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите первое число");
+int numberA = 6;
+Console.WriteLine("Введите второе число");
+int numberB = 4;
+Console.Write("Введите второе число");
+Console.Write(numberA + numberB);
